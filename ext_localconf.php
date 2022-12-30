f1130258881161b9da14f925f7ef5ac4cf92ae28
Mario Lubenka <mario.lubenka@googlemail.com>
2022-12-30 14:11:16 +0100

    [TASK] Adjust die condition for TYPO3 v12 support

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,5 @@
 <?php
-defined('TYPO3_MODE') || die();
+// TYPO3_MODE for TYPO3 v10 and below
+defined('TYPO3_MODE') or defined('TYPO3') or die();
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['id_stock_pictures']['connectors']['unsplash'] = \Ideative\IdUnsplashConnector\Connector\UnsplashConnector::class;
